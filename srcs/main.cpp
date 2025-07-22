54099e204e573e0a476d6d149f0f9ca671bac0a6
fateemaazaahraae <fatimazahraebazaz3@gmail.com>
2025-07-22 17:57:09 +0100

    adding mini server

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,73 +1,110 @@
-#include <iostream>
-#include <cstring>      // for memset
-#include <cstdlib>      // for exit
-#include <unistd.h>     // for close()
-#include <netinet/in.h> // for sockaddr_in
-#include <sys/socket.h> // for socket functions
-#include <arpa/inet.h>  // for inet_ntoa
+# include <iostream>
+# include <cstring>
+# include <cstdlib>
+# include <unistd.h>
+# include <netinet/in.h>
+# include <sys/socket.h>
+# include <arpa/inet.h>
 
-#define PORT 8080
-#define BUFFER_SIZE 1024
+# define PORT 9090
+# define BUFFER_SIZE 2000
 
-int main() {
-    // 1. Create a socket (IPv4, TCP)
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
-        std::cerr << "Socket creation failed\n";
-        return 1;
+int main()
+{
+    // -> 1- Create a socket server (IPv4, TCP)
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0)
+    {
+        std::cerr << "Server Socket Ceation Failed" << std::endl;
+        exit(EXIT_FAILURE);
     }
 
-    // 2. Allow address reuse (helps avoid "Address already in use" error)
-    int opt = 1;
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
-    // 3. Bind the socket to an IP and port
-    struct sockaddr_in address;
-    std::memset(&address, 0, sizeof(address));
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY; // 0.0.0.0 (listen on all interfaces)
-    address.sin_port = htons(PORT);
-
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        std::cerr << "Bind failed\n";
-        close(server_fd);
-        return 1;
+    // -> 2- Allow address reuse
+    int yes = 1;
+    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) < 0)
+    {
+        close(server_socket);
+        std::cerr << "setsockopt() failed" << std::endl;
+        exit(EXIT_FAILURE);
     }
 
-    // 4. Start listening
-    if (listen(server_fd, 3) < 0) {
-        std::cerr << "Listen failed\n";
-        close(server_fd);
-        return 1;
+    // -> 3 bind the socket to an IP and port
+    struct sockaddr_in server_address;
+    std::memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(PORT);
+    if (bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) < 0)
+    {
+        close(server_socket);
+        std::cerr << "Bind failed" << std::endl;
+        exit(EXIT_FAILURE);
     }
 
-    std::cout << "Server is listening on port " << PORT << "...\n";
-
-    // 5. Accept a client connection
-    socklen_t addrlen = sizeof(address);
-    int client_fd = accept(server_fd, (struct sockaddr*)&address, &addrlen);
-    if (client_fd < 0) {
-        std::cerr << "Accept failed\n";
-        close(server_fd);
-        return 1;
+    // -> 4 start lestening
+    if (listen(server_socket, 100) < 0)
+    {
+        close(server_socket);
+        std::cerr << "Listen failed" << std::endl;
+        exit(EXIT_FAILURE);
     }
 
-    std::cout << "Client connected from: " << inet_ntoa(address.sin_addr) << "\n";
+    std::cout << "The Server is listening on PORT " << PORT << "..." << std::endl;
 
-    // 6. Read data from the client
+    // -> 5 Accept a client connection
+    struct sockaddr_in client_address;
+    socklen_t client_address_length = sizeof(client_address);
+    int client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_address_length);
+    if (client_socket < 0)
+    {
+        close(server_socket);
+        std::cerr << "Accept failed" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // ONE NESSAGE
+    // -> 6 Read data from client 
+    // char buffer[BUFFER_SIZE];
+    // int bytes = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
+    // if (bytes < 0)
+    // {
+    //     close(server_socket);
+    //     close(client_socket);
+    //     std::cerr << "Receive failed" << std::endl;
+    //     exit(EXIT_FAILURE);
+    // }
+    // buffer[bytes] = '\0';
+
+    // std::cout << "Client says: " << buffer << std::endl;
+
+    // // -> 7 Send response
+
+    // const char* response = "Hello froooom the serveeer !!!! Whoooa";
+    // if (send(client_socket, response, std::strlen(response), 0) < 0)
+    // {
+    //     close(server_socket);
+    //     close(client_socket);
+    //     std::cerr << "Send failed" << std::endl;
+    //     exit(EXIT_FAILURE);
+    // }
+
+    // HANDLE MULTIPLE MESSAGES
     char buffer[BUFFER_SIZE];
-    int bytes_received = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
-    if (bytes_received > 0) {
-        buffer[bytes_received] = '\0';
-        std::cout << "Client says: " << buffer << "\n";
+    while (true)
+    {
+        std::memset(buffer, 0, BUFFER_SIZE);
+        int bytes = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
+        if (bytes <= 0)
+        {
+            std::cerr << "Receive failed or Client disconnected" << std::endl;
+            break;
+        }
+
+        std::cout << "Client says: " << buffer << std::endl;
     }
 
-    // 7. Send response
-    const char* message = "Hello from server!\n";
-    send(client_fd, message, std::strlen(message), 0);
-
-    // 8. Close sockets
-    close(client_fd);
-    close(server_fd);
+    close(client_socket);
+    close(server_socket);
+    std::cout << "Server closed" << std::endl;
     return 0;
 }
