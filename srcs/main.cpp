a21e671121dd2c840babfde0da690692eca47f8f
Fatima Zahrae Bazaz <133344215+fateemaazaahraae@users.noreply.github.com>
2025-07-28 16:08:24 +0100

    Integrating multiplexing with old code (-poll()-)

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,110 +1,108 @@
 # include <iostream>
 # include <cstring>
-# include <cstdlib>
+# include <string>
+# include <vector>
+# include <fcntl.h>
 # include <unistd.h>
-# include <netinet/in.h>
+# include <cstdlib>
 # include <sys/socket.h>
+# include <netinet/in.h>
 # include <arpa/inet.h>
+# include <poll.h>
+# include <exception>
 
 # define PORT 9090
+# define MAX_CLIENT 2000
 # define BUFFER_SIZE 2000
 
 int main()
 {
-    // -> 1- Create a socket server (IPv4, TCP)
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket < 0)
-    {
-        std::cerr << "Server Socket Ceation Failed" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    // -> 2- Allow address reuse
-    int yes = 1;
-    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) < 0)
-    {
-        close(server_socket);
-        std::cerr << "setsockopt() failed" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    // -> 3 bind the socket to an IP and port
-    struct sockaddr_in server_address;
-    std::memset(&server_address, 0, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = INADDR_ANY;
-    server_address.sin_port = htons(PORT);
-    if (bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) < 0)
-    {
-        close(server_socket);
-        std::cerr << "Bind failed" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    // -> 4 start lestening
-    if (listen(server_socket, 100) < 0)
-    {
-        close(server_socket);
-        std::cerr << "Listen failed" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    std::cout << "The Server is listening on PORT " << PORT << "..." << std::endl;
-
-    // -> 5 Accept a client connection
-    struct sockaddr_in client_address;
-    socklen_t client_address_length = sizeof(client_address);
-    int client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_address_length);
-    if (client_socket < 0)
-    {
-        close(server_socket);
-        std::cerr << "Accept failed" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    // ONE NESSAGE
-    // -> 6 Read data from client 
-    // char buffer[BUFFER_SIZE];
-    // int bytes = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
-    // if (bytes < 0)
-    // {
-    //     close(server_socket);
-    //     close(client_socket);
-    //     std::cerr << "Receive failed" << std::endl;
-    //     exit(EXIT_FAILURE);
-    // }
-    // buffer[bytes] = '\0';
-
-    // std::cout << "Client says: " << buffer << std::endl;
-
-    // // -> 7 Send response
-
-    // const char* response = "Hello froooom the serveeer !!!! Whoooa";
-    // if (send(client_socket, response, std::strlen(response), 0) < 0)
-    // {
-    //     close(server_socket);
-    //     close(client_socket);
-    //     std::cerr << "Send failed" << std::endl;
-    //     exit(EXIT_FAILURE);
-    // }
-
-    // HANDLE MULTIPLE MESSAGES
+    int serverSocket = -1, clientSocket = -1;
+    std::vector<struct pollfd> fds;
+    struct pollfd pollfd;
+    int numFd = 0;
     char buffer[BUFFER_SIZE];
-    while (true)
+    try
     {
-        std::memset(buffer, 0, BUFFER_SIZE);
-        int bytes = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
-        if (bytes <= 0)
+        serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (serverSocket < 0)
+            throw std::runtime_error("Server Socket Creation Failed");
+
+        int opt = 1;
+        if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+            throw std::runtime_error("Setsockopt() failed");
+        
+        struct sockaddr_in serverAddr;
+        std::memset(&serverAddr, 0, sizeof(serverAddr));
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_addr.s_addr = INADDR_ANY;
+        serverAddr.sin_port = htons(PORT);
+
+        std::cout << "SERVER START LISTENING ON IP " << inet_ntoa(serverAddr.sin_addr) << " PORT " << PORT << std::endl;
+        if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0)
+            throw std::runtime_error("Bind() failed");
+        
+        if (listen(serverSocket, MAX_CLIENT) < 0)
+            throw std::runtime_error("Listen() failed");
+        std::cout << "SERVER START LISTENING ON PORT " << ntohs(serverAddr.sin_port) << std::endl;
+
+        pollfd.fd = serverSocket;
+        pollfd.events = POLLIN;
+        pollfd.revents = 0;
+        fds.push_back(pollfd);
+        numFd = 1;
+        while (true)
         {
-            std::cerr << "Receive failed or Client disconnected" << std::endl;
-            break;
+            int poll_count = poll(fds.data(), numFd, -1);
+            if (poll_count < 0)
+                throw std::runtime_error("Poll() failed");
+            if (numFd >= MAX_CLIENT)
+                throw std::runtime_error("MAX_CLIENT limit reached");
+            
+            for (int i = 0; i < numFd; i++)
+            {
+                if (fds[i].revents & POLLIN)
+                {
+                    if (fds[i].fd == serverSocket)
+                    {
+                        struct sockaddr_in clientAddr;
+                        socklen_t clientLen = sizeof(clientAddr);
+                        clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientLen);
+                        if (clientSocket < 0)
+                            continue;
+                        std::cout << "New client connected: " << inet_ntoa(clientAddr.sin_addr) << " (fd = " << clientSocket << ")" << std::endl;
+
+                        pollfd.fd = clientSocket;
+                        pollfd.events = POLLIN;
+                        pollfd.revents = 0;
+                        fds.push_back(pollfd);
+                        numFd++;
+                    }
+                    else
+                    {
+                        std::memset(buffer, 0, BUFFER_SIZE);
+                        int bytes = recv(fds[i].fd, &buffer, BUFFER_SIZE - 1, 0);
+                        if (bytes <= 0)
+                        {
+                            std::cout << "Client disconnected (fd = " << fds[i].fd << ")" << std::endl;
+                            close(fds[i].fd);
+                            fds[i] = fds[numFd - 1];
+                            fds.pop_back();
+                            numFd--;
+                            i--;
+                        }
+                        else
+                        {
+                            buffer[bytes] = '\0';
+                            std::cout << "Received from client (fd = " << fds[i].fd << "): " << buffer << std::endl;
+                        }
+                    }
+                }
+            }
         }
-
-        std::cout << "Client says: " << buffer << std::endl;
     }
-
-    close(client_socket);
-    close(server_socket);
-    std::cout << "Server closed" << std::endl;
-    return 0;
+    catch(std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
 }
