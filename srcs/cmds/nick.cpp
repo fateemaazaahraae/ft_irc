e55801e2cde8024b014fca15f7dc2f446455cf41
fbazaz <fbazaz@c3r8p1.1337.ma>
2025-08-01 08:20:12 +0100

    NICK command

--- srcs/cmds/nick.cpp
+++ srcs/cmds/nick.cpp
@@ -26,23 +26,23 @@ void Server::handle_nick(Client& client, std::vector<std::string> &args)
 {
     if (!client.get_client_authe())
     {
-        send(client.get_client_fd(), (args[1] + ": You have to register!\n").c_str(), 28, 0);
+        send(client.get_client_fd(), "You have to register!\n", 22, 0);
         return ;
     }
     if (args.size() != 2)
     {
-        send(client.get_client_fd(), (args[1] + ": Bad arguments\n").c_str(), 20, 0);
+        send(client.get_client_fd(), "Bad arguments\n", 14, 0);
         return ;
     }
     if (isNickTaken(args[1]))
     {
-        send(client.get_client_fd(), (args[1] + ": Nickname is already in use\n").c_str(), 29, 0);
+        send(client.get_client_fd(), "Nickname is already in use\n", 27, 0);
         return ;
     }
     if (isValidNickName(args[1]))
     {
         client.set_client_nickname(args[1]);
-        send(client.get_client_fd(), (args[1] + ": Your nickname set upped successfully\n").c_str(), 43, 0);
+        send(client.get_client_fd(), "Your nickname set up successfully\n", 34, 0);
         std::cout << "Client set nickname" << std::endl;
     }
     else
